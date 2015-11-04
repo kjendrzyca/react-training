c308aae87d08317c86cd85f18e3ee40c684a4e19
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-11-04 23:54:41 +0100

    adding logic to recommend component

--- components/recommend.jsx
+++ components/recommend.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+const PropTypes = React.PropTypes;
 
 const Recommend = React.createClass({
+
+    propTypes: {
+        params: PropTypes.object.isRequired
+    },
+
+    getInitialState () {
+        return {
+            productName: '',
+            email: '',
+            text: ''
+        };
+    },
+
+    componentDidMount () {
+        this.setState({productName: this.props.params.name});
+    },
+
+    _onEmailChange (event) {
+        this.setState({email: event.target.value});
+    },
+
+    _onTextChange (event) {
+        this.setState({text: event.target.value});
+    },
+
+    _recommend () {
+        console.log('thank you for recommendation', this.state);
+    },
+
     render () {
         return (
             <div className="Recommend">
-                Thanks for recommending {this.props.params.name}!
+                <form>
+                    <input disabled type="text" value={this.props.params.name} />
+                    <input onChange={this._onEmailChange} type="email" />
+                    <textarea cols="30" onChange={this._onTextChange} rows="10" />
+                    <button className="btn btn-success" type="submit" onClick={this._recommend}>Recommend</button>
+                </form>
             </div>
         );
     }
