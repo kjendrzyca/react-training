3122a001a641108bc31426a1ccccdbec71cf7b9a
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-11-20 08:53:19 +0100

    removing routes from app.jsx

--- app.jsx
+++ app.jsx
@@ -2,12 +2,4 @@ import React from 'react';
 
 const mainContainer = document.getElementById('main-container');
 
-const routes = (
-    <Router history={createBrowserHistory()}>
-        <Route component={Shop} path="/" />
-        <Route component={Cart} path="/cart"  />
-        <Route component={Recommend} path="/recommend/:name" />
-    </Router>
-);
-
 React.render(routes, mainContainer);
