72cd204931ec73c9f3e4fba7be451cb720dc7984
kjendrzyca <kjendrzyca@gmail.com>
2016-11-07 22:26:28 +0100

    Adding new todos

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const Greetings = require('./components/greetings.js')
+import React from 'React'
+import ReactDOM from 'react-dom'
+import InputBox from './components/inputBox'
 
-const SecondComponent = (
-  <div>
-    <Greetings color='red' text='Hello from' />
-    <Greetings color='green' text='the second' />
-    <Greetings color='red' text='component' />
-  </div>
-)
+let i = 0
 
-ReactDOM.render(SecondComponent, document.getElementById('app'))
+const App = React.createClass({
+  getInitialState () {
+    return {
+      todoList: [
+        {id: ++i, text: 'buy groceries'},
+        {id: ++i, text: 'visit mom'},
+        {id: ++i, text: 'prepare dinner'}
+      ]
+    }
+  },
+
+  addTodo (newTodo) {
+    this.setState(state => {
+      state.todoList.push({id: ++i, text: newTodo})
+      return state
+    })
+  },
+
+  render () {
+    return (
+      <div className='App'>
+        <div>TODO:</div>
+        <div>{this.state.todoList.map(todo => <div key={todo.id}>{todo.text}</div>)}</div>
+        <InputBox addTodo={this.addTodo} />
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ components/inputBox.js
@@ -0,0 +1,35 @@
+const React = require('react')
+
+const InputBox = React.createClass({
+  propTypes: {
+    addTodo: React.PropTypes.func.isRequired
+  },
+
+  getInitialState () {
+    return {
+      text: ''
+    }
+  },
+
+  changeText (event) {
+    this.setState({text: event.target.value}, () => console.log(this.state))
+  },
+
+  addTodo () {
+    console.log('adding todo', this.state.text)
+    this.props.addTodo(this.state.text)
+    this.setState({text: ''})
+  },
+
+  render () {
+    return (
+      <div>
+        <span>New todo:</span>
+        <input type='text' onChange={this.changeText} value={this.state.text} />
+        <button onClick={this.addTodo}>Add</button>
+      </div>
+    )
+  }
+})
+
+export default InputBox
