c050b9bfcff361d9b78ac86587e8dbd4901f7ecd
kjendrzyca <kjendrzyca@gmail.com>
2016-11-09 23:01:11 +0100

    Adjusted toggleDone action

--- components/todoDetails.js
+++ components/todoDetails.js
@@ -3,22 +3,6 @@ import {Link} from 'react-router'
 
 import PersistenceService from '../services/persistenceService'
 
-const toggleDone = (todoId) => {
-  console.log('works!')
-  return
-  // this.setState(({todos}) => {
-  //   const todoToUpdateIndex = todos.findIndex(todo => todo.id === todoId)
-  //   const todoToUpdate = todos[todoToUpdateIndex]
-  //   todoToUpdate.done = !todoToUpdate.done
-  //
-  //   return [
-  //     ...todos.slice(0, todoToUpdateIndex),
-  //     ...todos.slice(todoToUpdateIndex),
-  //     todoToUpdate
-  //   ]
-  // }, () => PersistenceService.persist(this.state.todos))
-}
-
 const TodoDetails = React.createClass({
   propTypes: {
     params: React.PropTypes.object.isRequired
@@ -26,12 +10,27 @@ const TodoDetails = React.createClass({
 
   getInitialState () {
     return {
-      todo: {}
+      todo: {done: false}
     }
   },
 
   componentDidMount () {
     this.setState({todo: PersistenceService.getTodoById(this.props.params.id)})
+  },
+
+  toggleDone () {
+    PersistenceService.toggleDone(this.props.params.id, (error) => {
+      if (error) {
+        console.log(error)
+        return
+      }
+      this.setState(({todo}) => {
+        todo.done = !todo.done
+        return {
+          todo: todo
+        }
+      })
+    })
   },
 
   render () {
@@ -42,7 +41,7 @@ const TodoDetails = React.createClass({
         <div>Id: {todo.id}</div>
         <div>Text: {todo.text}</div>
         <div>Priority: {todo.priority}</div>
-        <div>Done: <input type='checkbox' checked={todo.done} onChange={() => toggleDone(todo.id)} /></div>
+        <div>Done: <input type='checkbox' checked={todo.done} onChange={this.toggleDone} /></div>
         <Link to='/'>CLOSE</Link>
       </div>
     )
--- services/persistenceService.js
+++ services/persistenceService.js
@@ -8,5 +8,26 @@ export default {
   getTodoById (id) {
     const todos = JSON.parse(window.localStorage.getItem('todos'))
     return todos.find(todo => todo.id === id)
+  },
+  toggleDone (todoId, callback) {
+    try {
+      const todos = JSON.parse(window.localStorage.getItem('todos'))
+      const todoToUpdateIndex = todos.findIndex(todo => todo.id === todoId)
+      const todoToUpdate = todos[todoToUpdateIndex]
+      todoToUpdate.done = !todoToUpdate.done
+
+      const updatedTodos = [
+        ...todos.slice(0, todoToUpdateIndex),
+        ...todos.slice(todoToUpdateIndex + 1),
+        todoToUpdate
+      ]
+
+      window.localStorage.setItem('todos', JSON.stringify(updatedTodos))
+    } catch (error) {
+      callback(error)
+      return
+    }
+
+    callback()
   }
 }
